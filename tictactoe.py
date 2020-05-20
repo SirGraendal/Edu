4e77a921c3aebd980418e16184a1d761b10a001a
Uwe Ziegler <SirGraendal@users.noreply.github.com>
2020-05-20 23:59:20 +0200

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+# write your code here
+row0 = ["X", "O", "X"]
+row1 = ["O", "X", "O"]
+row2 = ["X", "X", "O"]
+
+print(row0[0] + " " + row0[1] + " " + row0[2])
+print(row1[0] + " " + row1[1] + " " + row1[2])
+print(row2[0] + " " + row2[1] + " " + row2[2])
